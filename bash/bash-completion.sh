cf82910b086021e10ff9dca27ccc267b9e6c4be9
Artur Caliendo Prado <artur.prado@gmail.com>
2017-04-27 22:26:25 -0300

    Update bash-completion.sh

--- bash/bash-completion.sh
+++ bash/bash-completion.sh
@@ -1,3 +1,12 @@
+# To install:
+# brew install bash bash-completion
+# curl -o ~/.git-prompt.sh \
+#     https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+#
+# ... and add the following line to your ~/.bash_profile:
+#
+# source ~/.git-prompt.sh
+
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
 fi
